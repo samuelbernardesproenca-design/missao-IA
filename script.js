1cf8d18b3523540fbff04a59c60d5a5879aaad58
samuelbernardesproenca-design <samuel.bernardes.proenca@escola.pr.gov.br>
2026-08-14 09:24:10 -0300

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal  = document.querySelector(".caixa-principal");
+const caixaPerguntas  = document.querySelector(".caixa-perguntas");
+const caixaAlternativas  = document.querySelector(".caixa-resultado");
+const caixaResultado  = document.querySelector(".caixa-principal");
+const textoResultado  = document.querySelector(".texto-resultado");
+
+const pergunta = [
+    
+    "Perguntas 1",
+    
+    "Pergunta 2"
+
+]
